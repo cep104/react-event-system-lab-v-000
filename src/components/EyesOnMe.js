fb1fcba33e819d1d36a5ccfcefe906aa070a69a4
cep104 <candicelizabethpeters@gmail.com>
2021-01-06 09:49:57 -0600

    notes added

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React, { Component } from "react";
+
+export default class EyesOnMe extends Component {
+  //make the functions for the events here!
+  handleOnBlur = () => {
+    console.log("Hey! Eyes on me!");
+  };
+
+  handleOnFocus = () => {
+    console.log("Good!");
+  };
+  render() {
+    return (
+      <div>
+        {/* add the functions on the button */}
+        <button onBlur={this.handleOnBlur} onFocus={this.handleOnFocus}>
+          Focus!
+        </button>
+      </div>
+    );
+  }
+}
